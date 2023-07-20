ee89eb192b40f773003888e200e4514b73a170cd
Miguel Ceballos <mceballosleonides@gmail.com>
2023-07-20 09:20:08 -0600

    Add colums to tasks table

--- database/migrations/2023_07_20_151317_create_tasks_table.php
+++ database/migrations/2023_07_20_151317_create_tasks_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('tasks', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('page_id')->constrained()->cascadeOnDelete();
+            $table->string('description');
+            $table->foreignId('status_id');
             $table->timestamps();
         });
     }
