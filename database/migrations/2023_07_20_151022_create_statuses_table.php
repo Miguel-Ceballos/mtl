f690769e6e66bbc85a66a8bc33927eca466d6404
Miguel Ceballos <mceballosleonides@gmail.com>
2023-07-20 09:15:35 -0600

    Add colums to statuses table

--- database/migrations/2023_07_20_151022_create_statuses_table.php
+++ database/migrations/2023_07_20_151022_create_statuses_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('statuses', function (Blueprint $table) {
             $table->id();
+            $table->string('status')->unique();
+            $table->string('slug')->unique();
             $table->timestamps();
         });
     }
