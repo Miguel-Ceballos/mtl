83fd806c9d8825c842958864715577f37fc6cc2f
Miguel Ceballos <mceballosleonides@gmail.com>
2023-07-21 01:02:04 -0600

    Modified styles to create pages view

--- resources/views/admin/pages/create.blade.php
+++ resources/views/admin/pages/create.blade.php
@@ -1,12 +1,11 @@
-<x-sidebar>
-    <div class="flex flex-col w-full h-full items-center justify-center">
-        <div class="flex flex-col items-center justify-center w-3/4 h-3/4 rounded-lg">
+<x-sidebar class="flex justify-center py-36">
+    <div class="flex flex-col w-3/4 h-full sm:w-full items-center justify-center bg-gray-700 rounded-md pt-16">
             <div class="container xl:max-w-4xl mx-auto px-4">
                 <h1 class="text-xl md:text-2xl font-bold text-sky-100 text-center uppercase xl xl:text-4xl">Create a new page</h1>
-                <form method="POST" action="{{ route('page.store') }}" class="mt-10" novalidate>
+                <form method="POST" action="{{ route('page.store') }}" class="mt-16" novalidate>
                     @csrf
-                    <div class="mt-4">
-                        <x-input-label for="name" :value="__('Name')" />
+                    <div class="mt-10">
+                        <x-input-label for="name" :value="__('Name')" class="pl-1"/>
 
                         <x-text-input id="name" class="block mt-1 w-full"
                                       type="text"
@@ -17,11 +16,10 @@
                         <x-input-error :messages="$errors->get('name')" class="mt-2" />
                     </div>
 
-                    <x-primary-button class="mt-4">
+                    <x-primary-button class="my-10">
                         {{ __('Create') }}
                     </x-primary-button>
                 </form>
             </div>
-        </div>
     </div>
 </x-sidebar>
