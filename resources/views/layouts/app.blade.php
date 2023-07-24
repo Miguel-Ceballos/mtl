9da3ee668a22295ca172780f650939ff6a91d373
Miguel Ceballos <mceballosleonides@gmail.com>
2023-07-24 16:11:04 -0600

    Agrega script para el correcto funcionamiento de componentes de tailwind

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,6 +6,7 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
         <script src="https://cdn.tailwindcss.com"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.7.0/flowbite.min.js"></script>
 
         <title>{{ config('app.name', 'Laravel') }}</title>
 
