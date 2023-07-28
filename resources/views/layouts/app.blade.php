41aea6f5905b769e272b06b2069ef0ccac5f8fc5
Miguel Ceballos <mceballosleonides@gmail.com>
2023-07-28 13:22:09 -0600

    Agrega script para funcionamiento de input date

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,6 +7,7 @@
 
         <script src="https://cdn.tailwindcss.com"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.7.0/flowbite.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.7.0/datepicker.min.js"></script>
 
         <title>{{ config('app.name', 'Laravel') }}</title>
 
